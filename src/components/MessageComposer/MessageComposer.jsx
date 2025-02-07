700a4efbba47c44cb58fa967375486a73a982779
Shahariar Rijon <shahariar.rijon@gmail.com>
2025-02-07 13:09:56 +0600

    refresh block

--- src/components/MessageComposer/MessageComposer.jsx
+++ src/components/MessageComposer/MessageComposer.jsx
@@ -150,7 +150,19 @@ function MessageComposer() {
     });
   };
 
-  
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = ''; 
+      return '';
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
   
 
   const handleSendToFuture = async () => {
